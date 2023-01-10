ae29616d744ae1460db1b5ad89ec62e7608f0801
Yucel Olcay <yucel.olcay@cressoftconsultancy.com>
2023-01-10 21:35:21 +0300

    update proxy

--- src/api.js
+++ src/api.js
@@ -2,15 +2,14 @@
 const FIND_LOCATION_FROM_IP = 'https://get.geojs.io/v1/ip/geo.json';
 
 // https://www.geodatasource.com/
-const PROXY = 'https://safe-ridge-22036.herokuapp.com/';
+const PROXY = 'http://ec2-44-212-74-130.compute-1.amazonaws.com/';
 const GEODATA_KEY = process.env.REACT_APP_GEODATA_KEY;
 
 const FIND_NEIGHBORS_FROM_ISO = process.env.NODE_ENV === 'production'
   ? `${PROXY}https://api.geodatasource.com/v2/neighboring-countries`
   : 'http://localhost:8010/proxy/v2/neighboring-countries';
 
-export const getLocation = () => fetch(FIND_LOCATION_FROM_IP)
-  .then((res) => res.json());
+export const getLocation = () => fetch(FIND_LOCATION_FROM_IP).then((res) => res.json());
 
 export const getNeighbors = (countryISO) => {
   const headers = new Headers();
@@ -21,14 +20,19 @@ export const getNeighbors = (countryISO) => {
     headers,
   };
 
-  return fetch(`${FIND_NEIGHBORS_FROM_ISO}?key=${GEODATA_KEY}&country_code=${countryISO}`, requestOptions)
-    .then((res) => {
-      // console.log(res);
-      const p = res.json();
-      return p;
-    })
-    // eslint-disable-next-line no-unused-vars
-    .catch((error) => {
-    // console.log('API ERROR = ', error)
-    });
+  return (
+    fetch(
+      `${FIND_NEIGHBORS_FROM_ISO}?key=${GEODATA_KEY}&country_code=${countryISO}`,
+      requestOptions,
+    )
+      .then((res) => {
+        // console.log(res);
+        const p = res.json();
+        return p;
+      })
+      // eslint-disable-next-line no-unused-vars
+      .catch((error) => {
+        // console.log('API ERROR = ', error)
+      })
+  );
 };
